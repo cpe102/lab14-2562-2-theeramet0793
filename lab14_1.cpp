ccb18ab0db94b59a441d75d32cdf7e1b62a962a5
theeramet0793 <theerametmetha@gmail.com>
2020-02-25 16:43:26 +0700

    commit-1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,11 +1,29 @@
-include <iostream>
-using namespace std;
+# include <iostream>
+#include<string>
+#include<iomanip>
+ using namespace std;
 
 
 int main(){
 	//Write your code here.
-	
-	
-	
+	int a=5;
+	string b="A";
+	string &c=b;
+	int  *x=&a;
+	string *y=&b;
+	int **z=&x;
+	cout<<"\n\n\n";
+	cout<<setw(10)<<"a"<<setw(10)<<"b"<<setw(10)<<"c"<<setw(10)<<"x"<<setw(10)<<"y"<<setw(10)<<"z"<<"\n";
+	cout<<"----------------------------------------------------------------------------------------\n";
+	cout<<setw(10)<<a<<setw(10)<<b<<setw(10)<<c<<setw(10)<<*x<<setw(10)<<*y<<setw(10)<<*z<<"\n";
+	cout<<setw(10)<<&a<<setw(10)<<&b<<setw(10)<<&c<<setw(10)<<x<<setw(10)<<y<<setw(10)<<z<<"\n";
+    c="F";
+    cout<<setw(10)<<a<<setw(10)<<b<<setw(10)<<c<<setw(10)<<*x<<setw(10)<<*y<<setw(10)<<*z<<"\n";
+	*y="W";
+	cout<<setw(10)<<a<<setw(10)<<b<<setw(10)<<c<<setw(10)<<*x<<setw(10)<<*y<<setw(10)<<*z<<"\n";
+	*x=6;
+	cout<<setw(10)<<a<<setw(10)<<b<<setw(10)<<c<<setw(10)<<*x<<setw(10)<<*y<<setw(10)<<*z<<"\n";
+	**z=7;
+	cout<<setw(10)<<a<<setw(10)<<b<<setw(10)<<c<<setw(10)<<*x<<setw(10)<<*y<<setw(10)<<*z<<"\n";
 	return 0;
 }
